d2632483f81af2337200378a41495ac98e5c8181
Patrick McSweeny <patmcsweeny@gmail.com>
2019-05-03 23:20:19 -0400

    Improvements to paint calculator

--- 09/paint_calculator.rs
+++ 09/paint_calculator.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::io::Write;
 use std::str::FromStr;
 
-const SQUARE_FEET_PER_GALLON: f32 = 350.0;
+const SQUARE_FEET_PER_GALLON: u16 = 350;
 
 fn main() {
     let mut length_input = String::new();
@@ -15,7 +15,7 @@ fn main() {
         .read_line(&mut length_input)
         .expect("Failed to read input.");
 
-    let length = u16::from_str(length_input.trim()).expect("Please enter a valid number.");
+    let length = f32::from_str(length_input.trim()).expect("Please enter a valid number.");
 
     println!("How many feet is the width?");
     io::stdout().flush().expect("");
@@ -24,16 +24,17 @@ fn main() {
         .read_line(&mut width_input)
         .expect("Failed to read input.");
 
-    let width = u16::from_str(width_input.trim()).expect("Please enter a valid number.");
+    let width = f32::from_str(width_input.trim()).expect("Please enter a valid number.");
 
-    let square_feet: f32 = (length * width).into();
-    let gallons: f32 = (square_feet / SQUARE_FEET_PER_GALLON).into();
+    let square_feet: f32 = length * width;
+    let gallons: f32 = (square_feet / SQUARE_FEET_PER_GALLON as f32).into();
     let rounded_gallons = gallons.ceil();
 
-    let mut gallon_string = String::from("gallon");
-    if rounded_gallons > 1.0 {
-        gallon_string.push('s');
-    }
+    let gallon_string = if rounded_gallons > 1.0 {
+        String::from("gallons")
+    }else{
+        String::from("gallon")
+    };
 
     println!(
         "You will need to purchase {} {} of paint to to cover {} square feet.",
