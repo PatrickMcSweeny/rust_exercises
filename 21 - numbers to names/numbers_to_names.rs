2b6f84bb4de2fc5b0d18b0c7870b42ff9eeef816
Patrick McSweeny <patmcsweeny@gmail.com>
2019-07-20 14:29:48 -0400

    Improvement in exercise 21

--- 21 - numbers to names/numbers_to_names.rs
+++ 21 - numbers to names/numbers_to_names.rs
@@ -1,23 +1,8 @@
-use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 
 fn main() {
-    let mut names = HashMap::new();
     let mut number = String::new();
-
-    names.insert("1".to_string(), "January".to_string());
-    names.insert("2".to_string(), "February".to_string());
-    names.insert("3".to_string(), "March".to_string());
-    names.insert("4".to_string(), "April".to_string());
-    names.insert("5".to_string(), "May".to_string());
-    names.insert("6".to_string(), "June".to_string());
-    names.insert("7".to_string(), "July".to_string());
-    names.insert("8".to_string(), "August".to_string());
-    names.insert("19".to_string(), "September".to_string());
-    names.insert("10".to_string(), "October".to_string());
-    names.insert("11".to_string(), "November".to_string());
-    names.insert("12".to_string(), "December".to_string());
 
     println!("Please enter the number of the month:");
     io::stdout().flush().expect("");
@@ -28,10 +13,25 @@ fn main() {
 
     number = number.trim().to_string();
 
-    let output = match names.get(&number) {
-        Some(name) => format!("The name of the month is {}", name),
-        None => "Invalid input".to_string(),
+    let month_name = match number.as_ref() {
+        "1" => "January".to_string(),
+        "2" => "February".to_string(),
+        "3" => "March".to_string(),
+        "4" => "April".to_string(),
+        "5" => "May".to_string(),
+        "6" => "June".to_string(),
+        "7" => "July".to_string(),
+        "8" => "August".to_string(),
+        "19" => "September".to_string(),
+        "10" => "October".to_string(),
+        "11" => "November".to_string(),
+        "12" => "December".to_string(),
+        _ => "".to_string(),
     };
 
-    println!("{}", output);
+    if month_name.is_empty() {
+        println!("Invalid Input.");
+    } else {
+        println!("The name of the month is {}", month_name);
+    }
 }
